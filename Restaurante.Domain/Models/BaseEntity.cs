6dbca68b94333be9266098b599c2fc02ed1ad8ad
Flávio <flavio_r_o@yahoo.com.br>
2021-12-07 20:34:53 -0300

    Add project files.

---
+++ Restaurante.Domain/Models/BaseEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurante.Domain.Models
+{
+    internal class BaseEntity
+    {
+        public Guid Id { get; set; }
+        public DateTime DateCriated { get; set; }
+        public DateTime DateUpdated { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
